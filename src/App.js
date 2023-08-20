fa1e1077d05002dae818027f48e2e05a487dfe3e
rajon-git <rajon.zhsust15@gmail.com>
2023-08-20 23:34:24 +0600

    file folder add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './pages/main';
+
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Main/>
+      
     </div>
   );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Footer, Navbar } from '../components';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar/>
+
+
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
