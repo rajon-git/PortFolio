83cd22b6aee3aaf6217cdde73e14aa2a0835ccd8
rajon-git <rajon.zhsust15@gmail.com>
2023-08-20 23:37:49 +0600

    landing page add

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Footer, Navbar } from '../components';
+import Landing from '../components/Landing/Landing';
 
 const Main = () => {
     return (
@@ -7,6 +8,7 @@ const Main = () => {
             <Navbar/>
 
 
+            <Landing/>
             <Footer/>
         </div>
     );
