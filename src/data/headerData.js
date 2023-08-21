c162de0929eca40e1355abce03c512b8f9769d8c
rajon-git <rajon.zhsust15@gmail.com>
2023-08-21 21:41:22 +0600

    red theme add

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,6 +1,7 @@
 
+
 export const headerData = {
-    name: 'MD. Rajon Ahamad',
+    name: 'Jane Doe',
     title: "Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: 'https://avatars.githubusercontent.com/u/43471295?v=4',
